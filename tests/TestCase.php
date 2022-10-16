8b0a5308727c6bc9e2dbcf93602124b345c1ab0a
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 20:34:53 -0400

    Add basic test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spork\Research\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use Spork\Research\ResearchServiceProvider.php;
+
+class TestCase extends TestbenchTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Spork\\Research\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ResearchServiceProvider.php::class,
+        ];
+    }
+}
+
