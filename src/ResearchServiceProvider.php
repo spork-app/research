e0ea7ec3180ca09b3773091d071a9554a56ec520
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-11-24 23:45:07 -0500

    Update workflow merge the config

--- src/ResearchServiceProvider.php
+++ src/ResearchServiceProvider.php
@@ -10,7 +10,8 @@ class ResearchServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Spork::addFeature('research', 'BeakerIcon', '/research', 'tool');
+//        Spork::addFeature('research', 'BeakerIcon', '/research', 'tool');
+        $this->mergeConfigFrom(__DIR__ . '/../config/spork.php', 'spork.research');
 
         if (config('spork.research.enabled')) {
             Route::prefix('api')->group(__DIR__.'/../routes/api.php');
